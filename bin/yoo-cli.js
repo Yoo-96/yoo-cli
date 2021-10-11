be0e1853ea78afbb7f9bbf0b17a3e0507f2e30bd
luozongyong <156178793@qq.com>
2021-10-11 13:57:58 +0800

    chore: 修改bin命令路径

--- bin/yoo-cli.js
+++ bin/yoo-cli.js
@@ -9,11 +9,12 @@
 const program = require('commander');
 
 program
-  .version(require('../package').version)
-  .usage('<command> [options]')
-  .command('add', 'add a new template')
+  .version(require('../package').version) // 指定当前版本
+  .usage('<command> [options]') // 定义使用方式
+  .command('add', 'add a new template') // 定义指令
   .command('delete', 'delete a template')
   .command('list', 'list all the templates')
   .command('init', 'generate a new project from a template');
 
+// 解析命令行参数
 program.parse(program.argv);
